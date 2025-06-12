a624fae07334ee810d550fb2cdd7a0281e7f6803
aashi <aashiemail02@gmail.com>
2025-06-12 20:31:20 +0530

    making changes to app.py in port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request
 import pickle
 import numpy as np
+import os
 
 app=Flask(__name__)
 
@@ -46,4 +47,5 @@ def predict():
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use PORT from Render or default to 5000
+    app.run(host='0.0.0.0', port=port)
